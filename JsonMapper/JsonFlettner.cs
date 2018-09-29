dcc51948ce4e949c5eb892af885b9439d8761532
michael berezin <mbearz@gmail.com>
2018-09-29 10:59:29 +0300

    first push
    the first push,

---
+++ JsonMapper/JsonFlettner.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace JsonMapper
+{
+    public static class JsonFlettner
+    {
+        //https://www.newtonsoft.com/json
+        public static Dictionary<string, object> Flettner(string json)
+        {
+            dynamic fullObject = JsonConvert.DeserializeObject<ExpandoObject>(json, new ExpandoObjectConverter());
+
+            return GetInner(fullObject as ExpandoObject);
+        }
+
+        private static Dictionary<string, object> GetInner(ExpandoObject expandoObject)
+        {
+            var result = new Dictionary<string, object>();
+            var objectdictionary = expandoObject as IDictionary<string, object>;
+            Type expendoType = typeof(ExpandoObject);
+
+            foreach (var item in objectdictionary)
+            {
+                if (item.Value.GetType() != expendoType)
+                {
+                    result.Add(item.Key, item.Value);
+                }
+                else
+                {
+                    var innerResult = GetInner(item.Value as ExpandoObject);
+
+                    foreach (var innerItem in innerResult)
+                    {
+                        result.Add($"{item.Key}.{innerItem.Key}", innerItem.Value);
+                    }
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ JsonMapper/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace JsonMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folderPath = @"C:\Users\User\Documents\Visual Studio 2017\Projects\JsonMapper\JsonFlettner\JsonMapper\";
+            var sourceFile = "source.json";
+            var tragetFile = "target.json";
+
+            var jsonFile = File.ReadAllText(folderPath + sourceFile);
+
+            var flat = JsonFlettner.Flettner(jsonFile);
+
+
+            foreach (var item in flat)
+            {
+                Console.WriteLine(item.Key + " " + item.Value.ToString());
+            }
+
+            var targetJson = JsonConvert.SerializeObject(flat);
+
+            File.WriteAllText(folderPath + tragetFile, targetJson);
+
+            Console.Read();
+        }
+    }
+}
